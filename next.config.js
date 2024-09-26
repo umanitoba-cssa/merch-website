867fc74b19e83fa81c3cb8d1eed93f85e68365f2
okikiola fasanmi <62443604+kikibase@users.noreply.github.com>
2024-09-26 11:27:45 -0500

    Update and rename next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+// next.config.js
+module.exports = {
+  // ... rest of the configuration.
+  output: "standalone",
+};
